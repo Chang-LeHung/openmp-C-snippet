56833e307933f292ef8a28b6196f3ea4c6219d5d
Chang-LeHung <799207539@qq.com>
2022-10-29 23:48:32 +0800

    updatea makfile and fix verify.c compilation bug

--- task/verify.c
+++ task/verify.c
@@ -3,41 +3,110 @@
 #include <stdio.h>
 #include <omp.h>
 #include <unistd.h>
+#include <stdint.h>
+
+#define COLOR_NORMAL "\033[0m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREY "\033[1;30m"
+
+#define STACK_DEBUG printf("tid = %d"COLOR_RED" rsp = %lx"COLOR_NORMAL \
+                            COLOR_GREEN" rbp = %lx"COLOR_NORMAL"\n", \
+                            omp_get_thread_num(), rsp, rbp);
+
+#define FIND_RSP_RBP \
+        asm(\
+          "movq %%rbp, %0;"\
+          "movq %%rsp, %1;"\
+          :"=m"(rbp), "=m"(rsp)::\
+        );
+
+#define HLINE printf("===========================\n");
 
 int gpid;
+uint64_t rbp;
+uint64_t rsp;
 
 int main() {
-
   omp_set_num_threads(2);
   #pragma omp parallel
   {
     #pragma omp single
     {
+      HLINE
+    }
+    
+    #pragma omp critical
+    {
+      printf("at the beginning pid = %d\n", omp_get_thread_num());
+      FIND_RSP_RBP
+      STACK_DEBUG
+    }
+
+    #pragma omp barrier
+    #pragma omp single
+    {
+      HLINE
+    }
+    #pragma omp single
+    {
 
       #pragma omp task
       {
-        printf("finish task 1 && pid = %d\n", omp_get_thread_num());
-      }
-
-      #pragma omp task
-      {
-        printf("finish task 2 && pid = %d\n", omp_get_thread_num());
-      }
-
-      #pragma omp task shared(gpid) untied
-      {
-        gpid = omp_get_thread_num();
-        while(gpid == omp_get_thread_num()) {
-          sleep(1);
-          printf("pid = %d yield\n", omp_get_thread_num());
-          #pragma omp taskyield
+        #pragma omp critical
+        {
+          printf("task 1 pid = %d\n", omp_get_thread_num());
+          FIND_RSP_RBP
+          STACK_DEBUG
+          HLINE
         }
-        printf("finish task 3 && pid = %d && gpid = %d\n", omp_get_thread_num(), gpid);
       }
 
       #pragma omp task shared(gpid)
       {
-        printf("task 4 pid = %d\n", omp_get_thread_num());
+        #pragma omp critical
+        {
+          printf("task 2 pid = %d\n", omp_get_thread_num());
+          FIND_RSP_RBP
+          STACK_DEBUG
+          HLINE
+        }
+      }
+
+      #pragma omp task shared(gpid) untied
+      {
+        #pragma omp critical
+        {
+          printf("task 3 pid = %d\n", omp_get_thread_num());
+          FIND_RSP_RBP
+          STACK_DEBUG
+        }
+        int data = 100;
+        gpid = omp_get_thread_num();
+        while(gpid == omp_get_thread_num() && data >= 95) {
+          sleep(1);
+          data--;
+          printf("pid = %d yield data = %d\n", omp_get_thread_num(),
+                data);
+          FIND_RSP_RBP
+          STACK_DEBUG
+          #pragma omp taskyield
+        } 
+        FIND_RSP_RBP
+        STACK_DEBUG
+        HLINE
+      }
+
+      #pragma omp task shared(gpid)
+      {
+        #pragma omp critical
+        {
+          printf("task 4 pid = %d\n", omp_get_thread_num());
+          FIND_RSP_RBP
+          STACK_DEBUG
+          HLINE
+        }
       }
     }
   }
